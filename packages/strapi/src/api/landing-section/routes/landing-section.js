e6b1d146d509f7325a725c217b517bfd7a773bff
Gagan V <gagan@epesign.com>
2025-08-28 14:24:54 +0530

    ceated collection and added entry in strapi

---
+++ packages/strapi/src/api/landing-section/routes/landing-section.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * landing-section router
+ */
+
+// @ts-ignore
+const { createCoreRouter } = require("@strapi/strapi").factories;
+
+module.exports = createCoreRouter("api::landing-section.landing-section");
