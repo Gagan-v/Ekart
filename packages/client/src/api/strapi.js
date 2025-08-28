4b07ec7cc3a544c67dbd179578160a8fb3cdb1ac
Gagan V <gagan@epesign.com>
2025-08-28 19:55:36 +0530

    implement strapi cms data on landing

--- packages/client/src/api/strapi.js
+++ packages/client/src/api/strapi.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:1337/api";
-
 export const fetchLandingSections = async () => {
   try {
-    const res = await axios.get(`${API_BASE}/landing-section?populate=*`); //(?.) → avoids errors if image is missing.
+    const res = await axios.get(
+      "http://localhost:1337/api/landing-sections?populate=*"
+    ); //(?.) → avoids errors if image is missing.
     return res.data.data;
     // res.data → entire JSON from Strapi
     // res.data.data → array of collection entries(this is how strapi works)
   } catch (err) {
-    console.error(err);
-    return [];
+    console.error("AxiosError:", err);
+    throw err;
   }
 };
--- packages/strapi/config/middlewares.js
+++ packages/strapi/config/middlewares.js
@@ -1,12 +1,17 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:5173"], // your React dev server
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
