e122b4b06cea9c2d002ea0a3703dac741ad26f89
Gagan V <gagan@epesign.com>
2025-09-02 22:57:28 +0530

    added login auth and auth routes

--- packages/server/src/controllers/authController.js
+++ packages/server/src/controllers/authController.js
@@ -36,3 +36,33 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "server error", error: error.message });
   }
 };
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //check if user exits
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    //check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    //gen token
+    const token = jwt.sign(
+      { id: user._id }, //payload (data u want inside)
+      process.env.JWT_SECRET, //secret
+      { expiresIn: "7d" } //options
+    );
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error", error: error.message });
+  }
+};
