5b50bc7df92e570c748fb33609b6f57dc8624836
Gagan V <gagan@epesign.com>
2025-09-04 22:02:07 +0530

    added roues and auth controller for admin

---
+++ packages/server/src/controllers/adminAuthController.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import Admin from "../model/Admin.js";
+
+dotenv.config();
+
+export const adminLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    //check if admin exists
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.status(401).json({ messgae: "Invalid credentials" });
+    }
+    //check password
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Wrong passowrd" });
+    }
+    //genrate token
+    const token = jwt.sign(
+      { id: admin - _id, role: admin.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+    res.status(200).json({ message: "Admin Login Sucessful", token });
+  } catch (err) {
+    console.error("ADmin login error", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -24,6 +25,7 @@ app.get("/", (req, res) => {
 
 // Auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes); //link will be /admim/login(/login will come from adminRoutes)
 
 // DB connection
 connectDB().then(() => {
